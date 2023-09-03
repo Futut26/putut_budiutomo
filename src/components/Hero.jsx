11800e644948d83d8b5cb39a036c978b59999e67
Futut26 <putut.budiutomo@student.upj.ac.id>
2023-09-04 00:25:06 +0700

    hhh

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -20,7 +20,8 @@ const Hero = () => {
         </div>
       </div>
 
-      <ComputersCanvas/>
+    <ComputersCanvas/>
+      
 
 
       <div className="absolute xs:bottom-5  bottom-32 w-full flex justify-center items-center">
